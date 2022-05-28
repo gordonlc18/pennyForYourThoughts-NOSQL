870ff3cbaaa6ccac49ac06e77635b5fa348455d0
gordonlc18 <gordonlc18@gmail.com>
2022-05-28 12:10:18 -0400

    Removed outdated 'moment'code and changed file names for Insomnia.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const routes = require ('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,11 +11,9 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  
 });
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
